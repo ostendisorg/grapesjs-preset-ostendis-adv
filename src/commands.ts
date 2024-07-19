3685cc88e9681c8ad1f172587c6b96a89bd6e676
andy kramer <andy.kramer@ostendis.com>
2024-07-19 09:52:56 +0200

    Add ost block visibility command

--- src/commands.ts
+++ src/commands.ts
@@ -19,4 +19,15 @@ export default async (editor: Editor, opts: Required<PluginOptions>) => {
       run: (ed) => ed.setDevice('Mobile portrait'),
       stop: () => {},
     });
+
+    Commands.add('ost-blocks-visibility', {
+      run(editor) {
+        const cList = editor.Canvas.getBody().classList;
+        cList.add("show-ost-blocks");
+      },
+      stop(editor) {
+        const cList = editor.Canvas.getBody().classList;
+        cList.remove("show-ost-blocks");
+      },
+    });
 };
