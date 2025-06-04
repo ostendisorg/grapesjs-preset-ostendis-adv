a7b1fd3fb7f1bd91cebe17d746c33e329a86d8bc
AndyKramer <andy.kramer@ostendis.com>
2025-06-04 16:12:09 +0200

    Update to webpage & remove npm juice

--- src/pluginOptions.ts
+++ src/pluginOptions.ts
@@ -1,5 +1,3 @@
-import juice from "juice";
-
 export default interface PluginOptions {
   /**
    * Which blocks to add.
@@ -24,13 +22,6 @@ export default interface PluginOptions {
    * @default 'gjs-open-import-template'
    */
   cmdOpenImport?: string;
-
-  /**
-   * Get inlined HTML command id.
-   * @default 'gjs-get-inlined-html'
-   */
-  cmdInlineHtml?: string;
-
   /**
    * If `true`, inline CSS on export.
    * @default true
@@ -72,13 +63,6 @@ export default interface PluginOptions {
    * @default 'hopscotch'
    */
   codeViewerTheme?: string;
-
-  /**
-   * Custom options for `juice` HTML inliner.
-   * @default {}
-   */
-  juiceOpts?: juice.Options;
-
   /**
    * Ostendis translations
    */
